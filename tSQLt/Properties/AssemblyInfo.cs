2ccf7e43557538d1151edf5f56b9afd44636080a
Mordechai Kilstein <mordechai.kilstein@xpo.com>
2018-01-29 14:21:35 -0600

    SSDT DacPac
    Created project to generate dacpac file. Incorporated tSQLtCLR project and TestCaseFixes branch

---
+++ tSQLt/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("tSQLtCLR")]
+[assembly: AssemblyDescription("CLRs for the tSQLt unit testing framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sqlity.net")]
+[assembly: AssemblyProduct("tSQLt")]
+[assembly: AssemblyCopyright("Copyright ©  sqlity.net 2010 - <#=DateTime.Now.Year#>")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tSQLt/Stored Procedures/tSQLt/Private_ApplyForeignKeyConstraint.sql
@@ -0,0 +1,42 @@
+﻿
+CREATE PROCEDURE tSQLt.Private_ApplyForeignKeyConstraint 
+  @ConstraintObjectId INT,
+  @NoCascade BIT
+AS
+BEGIN
+  DECLARE @SchemaName NVARCHAR(MAX);
+  DECLARE @OrgTableName NVARCHAR(MAX);
+  DECLARE @TableName NVARCHAR(MAX);
+  DECLARE @ConstraintName NVARCHAR(MAX);
+  DECLARE @CreateFkCmd NVARCHAR(MAX);
+  DECLARE @AlterTableCmd NVARCHAR(MAX);
+  DECLARE @CreateIndexCmd NVARCHAR(MAX);
+  DECLARE @FinalCmd NVARCHAR(MAX);
+  
+  SELECT @SchemaName = SchemaName,
+         @OrgTableName = OrgTableName,
+         @TableName = TableName,
+         @ConstraintName = OBJECT_NAME(@ConstraintObjectId)
+    FROM tSQLt.Private_GetQuotedTableNameForConstraint(@ConstraintObjectId);
+      
+  SELECT @CreateFkCmd = cmd, @CreateIndexCmd = CreIdxCmd
+    FROM tSQLt.Private_GetForeignKeyDefinition(@SchemaName, @OrgTableName, @ConstraintName, @NoCascade);
+  SELECT @AlterTableCmd = 'ALTER TABLE ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + 
+                          ' ADD ' + @CreateFkCmd;
+  SELECT @FinalCmd = @CreateIndexCmd + @AlterTableCmd;
+
+  EXEC tSQLt.Private_RenameObjectToUniqueName @SchemaName, @ConstraintName;
+  IF (	SELECT	CASE transaction_isolation_level 
+				WHEN 0 THEN 'Unspecified' 
+				WHEN 1 THEN 'ReadUncommitted' 
+				WHEN 2 THEN 'ReadCommitted' 
+				WHEN 3 THEN 'Repeatable' 
+				WHEN 4 THEN 'Serializable' 
+				WHEN 5 THEN 'Snapshot' END AS TRANSACTION_ISOLATION_LEVEL 
+		FROM	sys.dm_exec_sessions 
+		WHERE	session_id = @@SPID 
+	  ) = 'Snapshot'
+    PRINT 'Not created due to transaction_isolation_level = Snapshot: ' + @FinalCmd;
+  ELSE
+    EXEC (@FinalCmd);
+END;
